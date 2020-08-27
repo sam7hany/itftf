b4637ed5b341f9b69c35ae5cc743ee59b266c8ea
sameh <39744797+sameh20130@users.noreply.github.com>
2020-08-27 16:43:20 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class ProgressBar
+{
+    private int _lastOutputLength;
+    private readonly int _maximumWidth;
+
+    public ProgressBar(int maximumWidth)
+    {
+        _maximumWidth = maximumWidth;
+        Show(" [ ");
+    }
+
+    public void Update(double percent)
+    {
+        // Remove the last state           
+        string clear = string.Empty.PadRight(_lastOutputLength, '\b');
+
+        Show(clear);
+
+        // Generate new state           
+        int width = (int)(percent / 100 * _maximumWidth);
+        int fill = _maximumWidth - width;
+        string output = string.Format("{0}{1} ] {2}%", string.Empty.PadLeft(width, '■'), string.Empty.PadLeft(fill, ' '), percent.ToString("0.0"));
+        Show(output);
+        _lastOutputLength = output.Length;
+    }
+
+    private void Show(string value)
+    {
+        Console.SetCursorPosition((Console.WindowWidth - value.Length) / 2, Console.CursorTop);
+
+        Console.Write(value);
+    }
+}
